5618c1aacfab0d058f286f1e2f2538d2adccb958
Gonzalo Bonora <gebonora@gmail.com>
2020-09-21 18:12:44 -0300

    More props

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className={"ui container comments"}>
-      <CommentDetail author={"Ricardo"} />
-      <CommentDetail author={"Sam"} />
-      <CommentDetail author={"Gandalf"} />
+      <CommentDetail
+        author={"Ricardo"}
+        timeAgo={"Today at 6:00AM"}
+        content={"Hello there!"}
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={"Sam"}
+        timeAgo={"Today at 5:32PM"}
+        content={"General Kenobi."}
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author={"Gandalf"}
+        timeAgo={"Yesterday at 10:32PM"}
+        content={"Nice Blog Post!"}
+        image={faker.image.avatar()}
+      />
     </div>
   );
 };
