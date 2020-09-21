72cd937a08902c804220082d0be03330d155f7ac
Gonzalo <gebonora@gmail.com>
2020-09-21 15:26:29 -0300

    jsx limitations

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ function getButtonText() {
   return "Click Me!";
 }
 
+/*
+jsx cannot show plain Objects, we can use .text to cheat it
+We can use objects in jsx as properties, eg in style, as long as they are not being printed
+ */
+
 const App = () => {
+  const nameText = "Enter name:";
   return (
     <div>
       <label className="label" for="name">
-        Enter name:
+        {nameText}
       </label>
       <input id="name" type="text" />
       <button style={{ color: "white", backgroundColor: "blue" }}>
