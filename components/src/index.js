6497cc7729027d972dcfb4fe0ef9e24f15301092
Gonzalo Bonora <gebonora@gmail.com>
2020-09-21 18:36:47 -0300

    Props children

--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className={"ui container comments"}>
-      <CommentDetail
-        author={"Ricardo"}
-        timeAgo={"Today at 6:00AM"}
-        content={"Hello there!"}
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={"Sam"}
-        timeAgo={"Today at 5:32PM"}
-        content={"General Kenobi."}
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={"Gandalf"}
-        timeAgo={"Yesterday at 10:32PM"}
-        content={"Nice Blog Post!"}
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        <div>Are you sure?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={"Ricardo"}
+          timeAgo={"Today at 6:00AM"}
+          content={"Hello there!"}
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={"Sam"}
+          timeAgo={"Today at 5:32PM"}
+          content={"General Kenobi."}
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={"Gandalf"}
+          timeAgo={"Yesterday at 10:32PM"}
+          content={"Nice Blog Post!"}
+          image={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
