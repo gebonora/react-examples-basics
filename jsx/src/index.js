e0e5e17d66e2aaec2f52466d5a7d75111c7c6789
Gonzalo <gebonora@gmail.com>
2020-09-21 16:11:11 -0300

    folder changes

---
+++ jsx/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function getButtonText() {
+    return "Click Me!";
+}
+
+/*
+jsx cannot show plain Objects, we can use .text to cheat it
+We can use objects in jsx as properties, eg in style, as long as they are not being printed
+ */
+
+const App = () => {
+    const nameText = "Enter name:";
+    return (
+        <div>
+            <label className="label" for="name">
+                {nameText}
+            </label>
+            <input id="name" type="text" />
+            <button style={{ color: "white", backgroundColor: "blue" }}>
+                {getButtonText()}
+            </button>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
