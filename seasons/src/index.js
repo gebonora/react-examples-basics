8184e83366e8d58e24d3aff277ea4ac13db439b7
Gonzalo Bonora <gebonora@gmail.com>
2020-09-22 14:57:02 -0300

    Error handling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,19 +5,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     // we ONLY assign to state in the constructor, everything else MUST be done with setState()
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
-      (error) => console.log(error)
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
     );
   }
 
   //we need to define render for React to work
   render() {
-    return <div>Location {this.state.lat} </div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
