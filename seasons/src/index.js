a55b329fc6ebd87e85ce75fa7e2e755c3d9685b7
Gonzalo Bonora <gebonora@gmail.com>
2020-09-22 15:03:59 -0300

    Conditional rendering

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -19,13 +19,13 @@ class App extends React.Component {
 
   //we need to define render for React to work
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading</div>;
   }
 }
 
