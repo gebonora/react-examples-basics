51202253a55cea0af5d0199ffadb1e93328e3c5e
Gonzalo Bonora <gebonora@gmail.com>
2020-09-22 15:53:55 -0300

    Icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-const SeasonDisplay = (lat) => {
-  console.log(lat);
-  return <div>disp</div>;
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? "Summer" : "Winter";
+  } else {
+    return latitude > 0 ? "Winter" : "Summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text =
+    season === "Winter" ? "Burr, it's chilly." : "Let's hit the beach";
+  const icon = season === "Winter" ? "snowflake" : "sun";
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
