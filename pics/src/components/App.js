28fec068af231c469bc77e8d43971133fcaf583d
Gonzalo Bonora <gebonora@gmail.com>
2020-09-23 12:18:10 -0300

    Refactor async-await to arrow function. State added.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,8 +3,9 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: term,
@@ -13,13 +14,15 @@ class App extends React.Component {
         Authorization: "Client-ID ujzoc5p8qQsmdMBqLSbJMJWT-CkrDizZFhS5MAtKyLo",
       },
     });
-    console.log(response.data.results);
-  }
+    console.log(this);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className={"ui container"} style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
       </div>
     );
   }
