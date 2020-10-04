4ecdd599e6b2a9dc4513803cfb7a1b50febca91d
Gonzalo Bonora <gebonora@gmail.com>
2020-10-04 00:28:57 -0300

    ImageCard sets its css spans.

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  //We destructure the object, referencing only the fields that we want.
-  const images = props.images.map(({ description, id, urls }) => (
-    <img key={id} src={urls.regular} alt={description} />
+  const images = props.images.map((image) => (
+    <ImageCard image={image} key={image.id} />
   ));
-  return <div>{images}</div>;
+  return <div className={"image-list"}>{images}</div>;
 };
 
 export default ImageList;
