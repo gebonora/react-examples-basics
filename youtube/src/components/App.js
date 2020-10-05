ce6b5ae954cf9ad9fd9871fb179ebc7546b9fe46
Gonzalo Bonora <gebonora@gmail.com>
2020-10-05 16:28:56 -0300

    Progress. Nested callbacks from child to parent.

--- youtube/src/components/App.js
+++ youtube/src/components/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+    console.log(response);
+  };
+
+  onVideoSelect = (video) => {
+    console.log("From the app!", video);
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className={"ui container"}>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
