6d50a0499b4625194892b3a00c18cfcc2a7baa2c
Gonzalo Bonora <gebonora@gmail.com>
2020-10-09 12:36:47 -0300

    Debouncing search.

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const options = [
   { label: "The Color Red", value: "red" },
@@ -22,21 +23,9 @@ const items = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      {/*<Accordion items={items} />*/}
-      {/*<Search />*/}
-      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle.</button>
-      {showDropdown ? (
-        <Dropdown
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
