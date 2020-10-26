5d5664c26c46fbebfe24b0cc3692788cea3784d6
Gonzalo Bonora <gebonora@gmail.com>
2020-10-26 18:37:35 -0300

    UserHeader

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,18 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// Usamos thunk. Funcion que retorna una funcion.
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
-
   dispatch({
     type: "FETCH_POSTS",
+    payload: response.data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: "FETCH_USER",
     payload: response.data,
   });
 };
