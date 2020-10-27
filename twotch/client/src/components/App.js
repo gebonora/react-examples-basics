809676bfa4006509fedf63b3ca1c477bfeec97bc
Gonzalo Bonora <gebonora@gmail.com>
2020-10-27 14:45:25 -0300

    Header

--- twotch/client/src/components/App.js
+++ twotch/client/src/components/App.js
@@ -5,13 +5,15 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className={"ui container"}>
       <BrowserRouter>
         <div>
-          <Route path={""} exact component={StreamList} />
+          <Header />
+          <Route path={"/"} exact component={StreamList} />
           <Route path={"/streams/new"} exact component={StreamCreate} />
           <Route path={"/streams/edit"} exact component={StreamEdit} />
           <Route path={"/streams/delete"} exact component={StreamDelete} />
