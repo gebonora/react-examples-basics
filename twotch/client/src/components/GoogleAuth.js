15200479a9740cde02579ced6c440daae8e8db15
Gonzalo Bonora <gebonora@gmail.com>
2020-11-03 15:42:06 -0300

    Oauth wiring

---
+++ twotch/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "370710553896-53vs3dvivtcjnusavjcnlejpe7htdf2d.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
