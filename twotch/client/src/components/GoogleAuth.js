dc8d5dc83501baf4536b1e587c340b0dacd80d38
Gonzalo Bonora <gebonora@gmail.com>
2020-11-06 11:15:41 -0300

    Auth

--- twotch/client/src/components/GoogleAuth.js
+++ twotch/client/src/components/GoogleAuth.js
@@ -1,18 +1,39 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "370710553896-53vs3dvivtcjnusavjcnlejpe7htdf2d.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "370710553896-53vs3dvivtcjnusavjcnlejpe7htdf2d.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
+  }
+  // arrow because its callback
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>IDK</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    }
+    return <div>not signed in</div>;
   }
 
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
