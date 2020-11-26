17f47b210a2bb4eb2d0ee8b6b633709d9d6f80c5
Gonzalo Bonora <gebonora@gmail.com>
2020-11-26 17:21:38 -0300

    Rendering list of streams

--- twotch/client/src/components/streams/StreamList.js
+++ twotch/client/src/components/streams/StreamList.js
@@ -7,9 +7,32 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className={"item"} key={stream.id}>
+          <i className={"large middle aligned icon camera"} />
+          <div className={"content"}>
+            {stream.title}
+            <div className={"description"}>{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Stream List</div>;
+    return (
+      <div>
+        <h2>Stream</h2>
+        <div className={"ui celled list"}>{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) }; // Object.values crea un array con los valores del objeto
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
