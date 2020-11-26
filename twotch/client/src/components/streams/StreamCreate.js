e81525cd67339dced89b8414c6fc2be70415ce01
Gonzalo Bonora <gebonora@gmail.com>
2020-11-26 14:18:25 -0300

    Styling and onSubmit

--- twotch/client/src/components/streams/StreamCreate.js
+++ twotch/client/src/components/streams/StreamCreate.js
@@ -2,18 +2,36 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({input}) {
+  renderInput({ input, label }) {
     //takes all properties on formProps and puts them as properties of the input
     return (
-      <input {...input} />
+      <div className={"field"}>
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     );
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form>
-        <Field name={"title"} component={this.renderInput} />
-        <Field name={"description"} component={this.renderInput} />
+      <form
+        className={"ui form"}
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field
+          name={"title"}
+          component={this.renderInput}
+          label={"Enter Title"}
+        />
+        <Field
+          name={"description"}
+          component={this.renderInput}
+          label={"Enter Description"}
+        />
       </form>
     );
   }
