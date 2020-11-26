2302316a3224105bbcffed5268ae517b318b51e5
Gonzalo Bonora <gebonora@gmail.com>
2020-11-26 17:03:01 -0300

    Added reducers

--- twotch/client/src/reducers/index.js
+++ twotch/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form"; //Cambiamos el nombre del export para expresividad
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
---
+++ twotch/client/src/reducers/streamReducer.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAMS:
+      return {...state, ..._.mapKeys(action.payload, "id")};
+    //crea un objeto a partir del array (1er arg), poniendo como keys la key del elemento especificada
+    // en el segundo arg como string. En este caso tenemos un array de objetos que tienen una key "id"
+    // y le creamos una key al objeto con el valor de id, y le asignamos el objeto.
+    case DELETE_STREAM:
+      return _.omit(state, action.payload); //crea un nuevo objeto omitiendo el segudo arg
+    default:
+      return state;
+  }
+};
