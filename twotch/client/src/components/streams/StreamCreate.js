ffdcdcffa3fb102bfe85ddd8d418c183e71d9bf9
Gonzalo Bonora <gebonora@gmail.com>
2020-11-26 15:43:59 -0300

    Creating action to post new stream

--- twotch/client/src/components/streams/StreamCreate.js
+++ twotch/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -13,7 +15,6 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = ({ input, label, meta }) => {
-    console.log(meta);
     //takes all properties on formProps and puts them as properties of the input
     const className = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
@@ -25,10 +26,10 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
+  onSubmit = (formValues) => {
     //handleSubmit from Redux-form does the preventDefault and sends the Fields in formValues as arg.
-    console.log(formValues);
-  }
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -64,7 +65,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
