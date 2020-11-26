bcd0a0b2a70b3b2728f66d5eaea552477968b817
Gonzalo Bonora <gebonora@gmail.com>
2020-11-26 13:53:03 -0300

    Using redux form

--- twotch/client/src/reducers/index.js
+++ twotch/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form"; //Cambiamos el nombre del export para expresividad
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
