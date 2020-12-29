a56af96e22fbffe41baea99d23b02e8ae619a50e
Gonzalo Bonora <gebonora@gmail.com>
2020-12-29 13:55:08 -0300

    Using context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,8 +1,14 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+  // static adds a property directly to the class itself.
+  // equals to doing: Button.contextType = LanguageContext; outside the class.
+
   render() {
-    return <button className={"ui button primary"}>Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return <button className={"ui button primary"}>{text}</button>;
   }
 }
 
